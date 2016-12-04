f898175eb87365624821f130e5eaf6b63af39624
Perry Franklin <Perry Franklin>
2016-12-04 20:30:57 +0100

    Policy iteration, with cute solution to inverse problems

--- PolicyIteration.m
+++ PolicyIteration.m
@@ -31,5 +31,48 @@ function [ J_opt, u_opt_ind ] = PolicyIteration( P, G )
 
 % put your code here
 
+
+iteration = 1;
+[m,n] = size(G);
+J_old = zeros(m);
+u_opt_ind = ones(m,1)*5; %literally just go north for everything
+
+while iteration < 100
+    
+    
+    u_old = u_opt_ind;
+    
+    for i = 1:m
+        for j = 1:m
+            pij(i,j) = P(i,j, u_opt_ind(i));
+        end
+        g(i,1) = G(i,u_opt_ind(i));
+    end
+    
+    J = inv(eye(m) - pij)*g;
+    
+    
+    for i = 1:m
+        temp_Ji = zeros(1,5);
+        for u = 1:n
+            sum = 0;
+            for j = 1:m
+                sum = sum + P(i,j,u)*J(j);
+            end
+            temp_Ji(u) = G(i,u) + sum;
+        end
+        [J(i), u_opt_ind(i)] = min(temp_Ji);
+    end
+
+    if isequal(u_old, u_opt_ind)
+        break;
+    end
+    
+    iteration = iteration+1
+end
+
+J_opt = J;
+
+
 end
 
